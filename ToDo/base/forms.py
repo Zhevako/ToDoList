4603ff9698510aa2958d3fdf3a6741db1f12440d
Ivan <ivanzhevako127@gmail.com>
2023-06-29 13:33:14 +0700

    Прописали стили

--- ToDo/base/forms.py
+++ ToDo/base/forms.py
@@ -3,6 +3,9 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    # title = forms.CharField(label="Имя")
+    # description = forms.CharField(label='Описание')
+    # date = forms.DateTimeField(label="Дата")
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = ['title', 'description']
